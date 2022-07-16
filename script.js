13d17ec17b6490026a5c6f7fe78a0cc4c011a68b
weiqinghan <66492945+weiqinghan@users.noreply.github.com>
2022-07-16 14:15:20 -0400

    updates

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const toTop = () => window.scrollTo({top:0,behavior:'smooth'});
 
 window.onscroll = () => {
     if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-        alert('you hit bottom of page');
+        alert('Surprise! This is a javascript function that is triggered by hitting the bottom of the page. I am still working on the display of a cute animation. Check back later!');
     }
 }
